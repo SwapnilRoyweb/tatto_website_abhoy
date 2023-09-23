eced4011d16e56eab8ea17536553d709bc7f1a89
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-09-24 02:40:07 +0600

    footer and navbar totally ready some few link have to add in component add running process

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo & important picture/A TOUCH OF INK LOGO.png'
+import { FaHome } from 'react-icons/fa';
+import { SiAlchemy } from "react-icons/si";
+import { MdWorkspacePremium, MdContactMail } from "react-icons/md";
+import { RiContactsFill } from "react-icons/ri";
+import { GrContactInfo } from "react-icons/gr";
 
 const Navbar = () => {
 
     const navItems = <>
-        <li><a>Home</a></li>
-        <li><a>My Works</a></li>
-        <li><a>My Designs</a></li>
-        <li><a>About</a></li>
-        <li><a>Contact</a></li>
+        <li><Link to='' className='flex items-center'><FaHome/>Home</Link></li>
+        <li><Link to='' className='flex items-center'><SiAlchemy/>Designs</Link></li>
+        <li><Link to='' className='flex items-center'><MdWorkspacePremium/>My Works</Link></li>
+        <li><Link to='' className='flex items-center'><RiContactsFill/>About Me</Link></li>
+        <li><Link to='' className='flex items-center'><MdContactMail/>Contact</Link></li>
     </>
 
     return (
@@ -18,7 +24,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-32">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-1 shadow bg-base-100 rounded-box w-32">
                         {navItems}
                     </ul>
                 </div>
