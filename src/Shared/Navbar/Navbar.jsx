e24ccda92b6b7e5c261f604e5580ad1f5b52c8f8
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-04-14 01:25:40 +0600

    some changes applied

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import { GrContactInfo } from "react-icons/gr";
 const Navbar = () => {
 
     const navItems = <>
-        <li><Link to='' className='flex items-center'><FaHome />Home</Link></li>
+        <li><Link to='' className='flex items-center'><FaHome /><span>Home</span></Link></li>
         <li><Link to='' className='flex items-center'><SiAlchemy />Designs</Link></li>
         <li><Link to='/myWorks' className='flex items-center'><MdWorkspacePremium />My Works</Link></li>
         <li><Link to='/about' className='flex items-center'><RiContactsFill />About Me</Link></li>
@@ -35,7 +35,7 @@ const Navbar = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden mr-16">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-1 shadow bg-black fixed bg-opacity-30 rounded-box w-32">
+                    <ul tabIndex={0} className="menu dropdown-content mt-3 z-[1] p-1 shadow bg-black bg-opacity-30 rounded-box w-32 py-5">
                         {navItems}
                     </ul>
                 </div>
