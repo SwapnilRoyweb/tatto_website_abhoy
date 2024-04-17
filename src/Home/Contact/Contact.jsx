ecaab71dc9d2d1c32fb9c87ad7a5ceb86276f5ce
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-04-18 01:38:18 +0600

    contact page working is running

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const Contact = () => {
     return (
         <div>
-            
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <div>
+                        <h1 className="text-5xl font-bold">Contact with me!</h1>
+                        <p className="py-6">Anytime if you need me, Don't feel shy to contact with me!</p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
