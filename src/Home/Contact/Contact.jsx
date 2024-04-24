0b0d1f74be5068e3d1479b28fa3f6163ff3f4f3c
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-04-24 22:34:54 +0600

    some changes applied which is necessary for pages are applied

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -6,17 +6,43 @@ import { Link } from 'react-router-dom';
 const Contact = () => {
     return (
         <div>
-            <div className="hero min-h-screen bg-gradient-to-r from-black to-slate-800 text-white">
+            <div className="hero min-h-screen bg-gradient-to-r from-black to-slate-800 text-white pb-8 lg:pb-0">
                 <div className="hero-content flex-col lg:flex-row-reverse gap-3 lg:gap-10">
-                    <img src={contactImg} className="max-w-xs rounded-lg shadow-2xl mt-10 lg:mt-0" />
+                    <img src={contactImg} className="max-w-xs rounded-lg shadow-2xl mt-14 lg:mt-0" />
                     <div>
-                        <h1 className="text-5xl font-bold">Contact with me!</h1>
-                        <p className="py-6">Anytime if you need me, Don't feel shy to contact with me!</p>
-                        <nav className="flex items-center justify-center gap-5">
+                        <div className='my-5'>
+                            <h1 className="text-5xl font-bold mt-5 text-center">Contact with me!</h1>
+                            <p className="py-3 text-center">Anytime if you need me, Don't feel shy to contact with me!</p>
+                        </div>
+                        <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 px-3">
+                            <form className="card-body">
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" placeholder="email" className="input input-bordered" required />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" placeholder="password" className="input input-bordered" required />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <button className="btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
+
+                        <nav className="flex items-center justify-center gap-5 mt-8">
                             <button className='btn btn-md btn-square outline outline-2 outline-blue-500 bg-transparent text-blue-500 hover:bg-blue-600 hover:text-white'><Link to='' className='text-2xl'><FaFacebook /></Link></button>
                             <button className='btn btn-md btn-square outline outline-2 outline-green-400 bg-transparent text-green-400 hover:bg-green-500 hover:text-white'><Link to='' className='text-2xl'><FaWhatsapp /></Link></button>
                             <button className='btn btn-md btn-square outline outline-2 outline-red-600 bg-transparent text-red-600 hover:bg-red-600 hover:text-white'><Link to='' className='text-2xl'><FaYoutube /></Link></button>
                         </nav>
+
                     </div>
                 </div>
             </div>
--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,12 +3,12 @@ import logo from '../../assets/logo & important picture/A TOUCH OF INK LOGO.png'
 
 const Footer = () => {
     return (
-        <footer className="footer mx-auto w-11/12 px-8 py-5 lg:py-3 rounded-xl bg-gradient-to-r from-black to-slate-800 text-neutral-content flex flex-col md:flex-row items-center justify-around my-8">
+        <footer className="footer mx-auto w-11/12 px-8 py-5 lg:py-3 rounded-xl bg-gradient-to-r from-black to-slate-800 text-neutral-content flex flex-row items-center justify-around my-8">
             <aside className="flex items-center">
                 <img src={logo} alt="" className='h-28' />
                 <p className='text-white text-2xl font-bold'>A Touch of INK</p>
             </aside>
-            <div className='hidden md:flex'>
+            <div className='hidden sm:flex'>
                 <p className='text-center'>Copyright © 2023 - All right reserved</p>
             </div>
         </footer>
