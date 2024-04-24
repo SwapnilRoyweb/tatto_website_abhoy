fc9eadab91905567900aa785ad24998a39af92cb
swapnilRoyWeb <swapnilroyweb@gmail.com>
2024-04-25 00:10:51 +0600

    more changes applied to contact page which is neccesary

--- src/Home/Contact/Contact.jsx
+++ src/Home/Contact/Contact.jsx
@@ -2,25 +2,28 @@ import React from 'react';
 import contactImg from '../../assets/logo & important picture/Banner/2148121930-removebg-preview.png'
 import { FaFacebook, FaWhatsapp, FaYoutube } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import Tilt from 'react-parallax-tilt';
 
 const Contact = () => {
     return (
         <div>
             <div className="hero min-h-screen bg-gradient-to-r from-black to-slate-800 text-white pb-8 lg:pb-0">
                 <div className="hero-content flex-col lg:flex-row-reverse gap-3 lg:gap-10">
-                    <img src={contactImg} className="max-w-xs rounded-lg shadow-2xl mt-14 lg:mt-0" />
+                    <Tilt>
+                        <img src={contactImg} className="max-w-xs rounded-lg shadow-2xl mt-20 lg:mt-0 bg-gradient-to-b from-cyan-900 to-gray-600" />
+                    </Tilt>
                     <div>
                         <div className='my-5'>
                             <h1 className="text-5xl font-bold mt-5 text-center">Contact with me!</h1>
                             <p className="py-3 text-center">Anytime if you need me, Don't feel shy to contact with me!</p>
                         </div>
-                        <div className="card shrink-0 glass w-full max-w-sm shadow-2xl bg-gradient-to-r from-[#FFDEE9] to-[#B5FFFC] px-3">
+                        <div className="card shrink-0 glass w-full max-w-sm shadow-2xl bg-gradient-to-r from-cyan-600 to-gray-700">
                             <form className="card-body">
                                 <div className="form-control">
                                     <label className="label">
                                         <span className="label-text text-black">Your Message</span>
                                     </label>
-                                    <input type="text" placeholder="Your Message to Abhoy" className="input input-bordered" required />
+                                    <input type="text" placeholder="Your Message to Abhoy" className="input input-bordered text-white" required />
                                 </div>
                                 <div className="form-control">
                                     <label className="label">
